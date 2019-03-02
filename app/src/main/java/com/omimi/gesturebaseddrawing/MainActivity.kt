24a65cdc71bead89e41cdb0d64ae0b83ebd83c82
Artem Sharypov <artem.sharypov@gmail.com>
2019-03-02 02:32:55 -0600

    Show/Hide Palette & Interpretation / Drawing
    -Added way to show/hide the palette for shapes and colours
    -Added way to switch between Interpreation and Drawing views

--- app/src/main/java/com/omimi/gesturebaseddrawing/MainActivity.kt
+++ app/src/main/java/com/omimi/gesturebaseddrawing/MainActivity.kt
@@ -2,12 +2,20 @@ package com.omimi.gesturebaseddrawing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ShapeDrawnCallback {
     private var currDrawingShapeType: DrawingShapeType = DrawingShapeType.DrawingLine
     private var currShapeColor: ShapeColor = ShapeColor.Blue
+    private var paletteVisible = true
+    private lateinit var drawingMenuItem: MenuItem
+    private lateinit var interpretationMenuItem: MenuItem
+    private lateinit var hidePaletteMenuItem: MenuItem
+    private lateinit var showPaletteMenuItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,37 +29,21 @@ class MainActivity : AppCompatActivity(), ShapeDrawnCallback {
         btn_line.backgroundTintList = ContextCompat.getColorStateList(this, R.color.selectedShapeType)
         btn_blue.backgroundTintList = ContextCompat.getColorStateList(this, R.color.selectedShapeType)
 
-        btn_triangle.setOnClickListener { view ->
-            drawingShapeTypeChanged(DrawingShapeType.DrawingTriangle)
-        }
+        btn_triangle.setOnClickListener { drawingShapeTypeChanged(DrawingShapeType.DrawingTriangle) }
 
-        btn_square.setOnClickListener { view ->
-            drawingShapeTypeChanged(DrawingShapeType.DrawingSquare)
-        }
+        btn_square.setOnClickListener { drawingShapeTypeChanged(DrawingShapeType.DrawingSquare) }
 
-        btn_line.setOnClickListener { view ->
-            drawingShapeTypeChanged(DrawingShapeType.DrawingLine)
-        }
+        btn_line.setOnClickListener { drawingShapeTypeChanged(DrawingShapeType.DrawingLine) }
 
-        btn_circle.setOnClickListener { view ->
-            drawingShapeTypeChanged(DrawingShapeType.DrawingCircle)
-        }
+        btn_circle.setOnClickListener { drawingShapeTypeChanged(DrawingShapeType.DrawingCircle) }
 
-        btn_blue.setOnClickListener { view ->
-            shapeColorChanged(ShapeColor.Blue)
-        }
+        btn_blue.setOnClickListener { shapeColorChanged(ShapeColor.Blue) }
 
-        btn_black.setOnClickListener { view ->
-            shapeColorChanged(ShapeColor.Black)
-        }
+        btn_black.setOnClickListener { shapeColorChanged(ShapeColor.Black) }
 
-        btn_orange.setOnClickListener { view ->
-            shapeColorChanged(ShapeColor.Orange)
-        }
+        btn_orange.setOnClickListener { shapeColorChanged(ShapeColor.Orange) }
 
-        btn_green.setOnClickListener { view ->
-            shapeColorChanged(ShapeColor.Green)
-        }
+        btn_green.setOnClickListener { shapeColorChanged(ShapeColor.Green) }
     }
 
     private fun drawingShapeTypeChanged(shapeType: DrawingShapeType) {
@@ -122,5 +114,74 @@ class MainActivity : AppCompatActivity(), ShapeDrawnCallback {
 
     override fun shapeDrawn(shape: Shape) {
         //todo just pass this shape into the other view once created
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+
+        drawingMenuItem = menu.findItem(R.id.drawing)
+        interpretationMenuItem = menu.findItem(R.id.interpretation)
+        hidePaletteMenuItem = menu.findItem(R.id.hide_palette)
+        showPaletteMenuItem = menu.findItem(R.id.show_palette)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.drawing -> {
+                //todo hide interpretation
+
+                cl_user_draw_input.visibility = View.VISIBLE
+
+                drawingMenuItem.isVisible = false
+                interpretationMenuItem.isVisible = true
+
+                if(paletteVisible) {
+                    hidePaletteMenuItem.isVisible = true
+                } else {
+                    showPaletteMenuItem.isVisible = true
+                }
+
+                true
+            }
+
+            R.id.interpretation -> {
+                //todo show interpreation
+                cl_user_draw_input.visibility = View.GONE
+                drawingMenuItem.isVisible = true
+                interpretationMenuItem.isVisible = false
+                showPaletteMenuItem.isVisible = false
+                hidePaletteMenuItem.isVisible = false
+
+                true
+            }
+
+            R.id.hide_palette -> {
+                cl_palette.visibility = View.GONE
+
+                paletteVisible = false
+
+                showPaletteMenuItem.isVisible = true
+                hidePaletteMenuItem.isVisible = false
+
+                true
+            }
+
+            R.id.show_palette -> {
+                cl_palette.visibility = View.VISIBLE
+
+                paletteVisible = true
+
+                showPaletteMenuItem.isVisible = false
+                hidePaletteMenuItem.isVisible = true
+
+                true
+            }
+
+            else -> {
+                 super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
